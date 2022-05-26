8578dc995bb528aee5149f6df01b5f737c8ebaec
paraswastaken <akarkaulia2000@gmail.com>
2022-05-26 21:23:50 +0530

    Functions in JS

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-console.log("Hello World")
-let a = 10
+function add7(n) {
+    return n+7;
+  }
+  function multiply(a, b) {
+    return a*b;
+  }
+  function capitalise(str) {
+    newstr = str.toLowerCase();
+    return newstr[0].toUpperCase() + newstr.slice(1)
+  }
+  function lastLetter(str) {
+    return str.slice(-1);
+  }
