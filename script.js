a446b9eb6a0020b10305d9db573d2524ebedf178
paraswastaken <akarkaulia2000@gmail.com>
2022-05-28 22:13:11 +0530

    Test DOM manipulation using script.js

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-function add7(n) {
-    return n+7;
-  }
-  function multiply(a, b) {
-    return a*b;
-  }
-  function capitalise(str) {
-    newstr = str.toLowerCase();
-    return newstr[0].toUpperCase() + newstr.slice(1)
-  }
-  function lastLetter(str) {
-    return str.slice(-1);
-  }
+const container = document.querySelector("#container");
+const content = document.createElement("div");
+content.textContent = "This is you glorious content div!!!";
+container.appendChild(content);
+content.classList.add('content');
+const red = document.createElement("p");
+red.textContent="Hey I'm red";
+red.style.color = "red";
+container.appendChild(red);
+const blu = document.createElement("h3");
+blu.textContent="I'm a blue h3!";
+blu.style.color = "blue";
+container.appendChild(blu);
+const sdiv = document.createElement("div");
+sdiv.style.borderStyle = "solid";
+sdiv.style.borderColor = "black";
+sdiv.style.backgroundColor = "pink";
+const inh1 = document.createElement("h1");
+inh1.textContent = "I'm in a div";
+const inp = document.createElement("p");
+inp.textContent="ME TOO!";
+sdiv.append(inh1, inp);
+container.appendChild(sdiv);
