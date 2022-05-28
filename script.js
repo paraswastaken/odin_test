6763fc1baf86ad086dd4c2f24b8d2310cd5712ae
paraswastaken <akarkaulia2000@gmail.com>
2022-05-28 22:44:08 +0530

    Test some more stuff

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const container = document.querySelector("#container");
 const content = document.createElement("div");
 content.textContent = "This is you glorious content div!!!";
 container.appendChild(content);
-content.classList.add('content');
+content.setAttribute('class', 'content');
 const red = document.createElement("p");
 red.textContent="Hey I'm red";
 red.style.color = "red";
@@ -15,9 +15,26 @@ const sdiv = document.createElement("div");
 sdiv.style.borderStyle = "solid";
 sdiv.style.borderColor = "black";
 sdiv.style.backgroundColor = "pink";
-const inh1 = document.createElement("h1");
+const inh1 = document.createElement("button");
 inh1.textContent = "I'm in a div";
 const inp = document.createElement("p");
 inp.textContent="ME TOO!";
 sdiv.append(inh1, inp);
 container.appendChild(sdiv);
+inh1.addEventListener('click', function (e) {
+  alert("HELLOOOOO!!");
+  console.log(e.target);
+  e.target.style.backgroundColor="blue";
+});
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll('button');
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+    alert(button.id);
+  });
+});
